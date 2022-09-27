71ca559078966507dd305bfaa417ae81a6b82d2d
wniebelski <wniebelski@gmail.com>
2022-09-27 10:00:54 +0200

    rozwiazanie

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function sprawdzanie (n1, n2){
+    if (n1 === 20 || n2 === 20){
+    console.log ('True');
+}else if (n1 + n2 <= 20){
+    console.log ('True');
+}else {
+    console.log ('False');
+}
+}
+sprawdzanie(10, 10)
